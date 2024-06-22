7e8caa13802a5f05380430e5be0b5c029724c1e4
llocr <36-96@naver.com>
2024-06-22 15:47:46 +0900

    feat/#25 : 찜하기 수 조회 기능 구현

--- src/main/java/like/heocholi/spartaeats/controller/AnalyticsController.java
+++ src/main/java/like/heocholi/spartaeats/controller/AnalyticsController.java
@@ -1,7 +1,14 @@
 package like.heocholi.spartaeats.controller;
 
+import like.heocholi.spartaeats.dto.ResponseMessage;
+import like.heocholi.spartaeats.security.UserDetailsImpl;
 import like.heocholi.spartaeats.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/stores/{storeId}/analytics")
 public class AnalyticsController {
-
     private final AnalyticsService analyticsService;
 
+    //가게별 찜하기 수 보기
+    @GetMapping("/picks/count")
+    public ResponseEntity<ResponseMessage<Long>> getPickCount(@PathVariable Long storeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long pickCount = analyticsService.getPickCount(storeId, userDetails.getManager());
 
+        ResponseMessage<Long> responseMessage = ResponseMessage.<Long>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("찜하기 수 조회가 완료되었습니다.")
+                .data(pickCount)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
 }
--- src/main/java/like/heocholi/spartaeats/service/AnalyticsService.java
+++ src/main/java/like/heocholi/spartaeats/service/AnalyticsService.java
@@ -1,10 +1,26 @@
 package like.heocholi.spartaeats.service;
 
+import like.heocholi.spartaeats.entity.Manager;
+import like.heocholi.spartaeats.entity.Pick;
+import like.heocholi.spartaeats.entity.Store;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AnalyticsService {
+    private final StoreService storeService;
 
+    //가게별 찜하기 수 보기
+    public Long getPickCount(Long storeId, Manager manager) {
+        Store store = storeService.findStore(storeId, manager);
+
+        List<Pick> pickList = store.getPickList();
+
+        return pickList.stream().filter(Pick::isPick).count();
+    }
 }
